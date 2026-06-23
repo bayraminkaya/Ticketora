fbc0a09f76b06459fb55608f254cec0eece32254
bayraminkaya <91468628+bayraminkaya@users.noreply.github.com>
2026-06-23 21:53:26 +0300

    Add project files.

---
+++ Infrastructure/Ticketora.Persistence/Context/TicketoraContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticketora.Domain.Entities;
+
+namespace Ticketora.Persistence.Context
+{
+    public class TicketoraContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-5V6MJ7S;Initial Catalog=TicketoraDb;integrated security=true;trust server certificate=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
